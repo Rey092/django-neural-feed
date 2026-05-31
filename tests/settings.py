965b92f2bf913709c41db7bf10911e253dd086f5
Dersty <glays.channel@gmail.com>
2026-05-31 14:54:46 +0500

    chore: setup pytest environment and configure pyproject.toml

---
+++ tests/settings.py
@@ -0,0 +1,18 @@
+import os
+
+SECRET_KEY = 'fake-secret-key-for-testing-purposes-only'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django_neural_feed', 
+]
+
+USE_TZ = True
