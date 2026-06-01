36bf2854c23e0854ea7598bd02fda92bbf2263f9
Dersty <glays.channel@gmail.com>
2026-06-01 17:43:08 +0500

    fix2

---
+++ tests/migrations/0001_vector_extension.py
@@ -0,0 +1,13 @@
+# tests/migrations/0001_vector_extension.py
+from django.db import migrations
+
+class Migration(migrations.Migration):
+
+    dependencies = []
+
+    operations = [
+        migrations.RunSQL(
+            "CREATE EXTENSION IF NOT EXISTS vector;",
+            reverse_sql="DROP EXTENSION IF EXISTS vector;",
+        ),
+    ]
