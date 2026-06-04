912a5a8b2a47300702828064c8c1064b6af5b73b
Dersty <glays.channel@gmail.com>
2026-06-04 11:28:25 +0500

    fix: test failing black check; chore: yaml file now using uv for downloading packages

--- tests/test_recommendations.py
+++ tests/test_recommendations.py
@@ -32,8 +32,7 @@ def test_calculate_embedding_calls_sentence_transformers_correctly(mocker):
     result = RecommendationService.calculate_embedding(test_text)
 
     mock_transformer_class.assert_called_once_with(
-        "paraphrase-multilingual-MiniLM-L12-v2", 
-        local_files_only=True
+        "paraphrase-multilingual-MiniLM-L12-v2", local_files_only=True
     )
     mock_model_instance.encode.assert_called_once_with(test_text, convert_to_numpy=True)
 
